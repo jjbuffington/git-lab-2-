1b34b96fa0ffd32841267a130d7917f8c8399e59
JJ Buffington <jb144518@ohio.edu>
2019-09-05 16:17:36 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: JJ Buffington
+ *        Date: 9/5/2019
+ * Description: git lab program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
